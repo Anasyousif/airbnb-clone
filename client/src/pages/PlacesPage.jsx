a066eef186231e001599a80cf1da539cc387df98
Anas Yousif <Anasyousif182@gmail.com>
2023-10-26 10:41:04 -0400

    adding a few features

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,179 +1,39 @@
-import { Link, useParams } from "react-router-dom";
-
+import {Link, useParams} from "react-router-dom";
+import AccountNav from "../AccountNav";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import PlaceImg from "../PlaceImg";
 export default function PlacesPage() {
-  const { action } = useParams();
-  console.log(action);
+  const [places,setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get('/user-places').then(({data}) => {
+      setPlaces(data);
+    });
+  }, []);
   return (
     <div>
-      {action === !"new" && (
+      <AccountNav />
         <div className="text-center">
-          <Link
-            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-            to={"/account/places/new"}
-          >
-            Add new place
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
+          <Link className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full" to={'/account/places/new'}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+              <path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
             </svg>
+            Add new place
           </Link>
         </div>
-      )}
-      {action === "new" && (
-        <form>
-          <h2 className="text-2xl mt-4">Title</h2>
-          <p className="text-gray-500 text-sm">
-            title for your place. should be short and catchy as ijn advertisment
-          </p>
-          <input
-            type="text"
-            placeholder="title, for example my lovely apartment"
-          />
-          <h2 className="text-2xl mt-4">Address to this place</h2>
-          <input type="text" placeholder="Address" />
-          <h2 className="text-2xl mt-4">Photos</h2>
-          <p className="text-2xl mt-4"> more = better</p>
-          <div className="flex gap-2">
-            <input type="text" placeholder={"Add using a link .... jpg"} />
-            <button className="bg-gray-200 px-4 rounded-2xl">
-              Add&nbsp;photo
-            </button>
-          </div>
-          <div className="mt-2 grid grid-cols-13 md:grid-cols-4 lg:grid-cols-6">
-            <button className="flex  gap-1 justify-center border bg-transparent rounded-2xl p-8 text-gary-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-8 h-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
-                />
-              </svg>
-              Upload from your device
-            </button>
-          </div>
-          <div>
-            <h2 className="text-2xl mt-4">Description</h2>
-            <p className="text-2xl mt-4"> Description of the place</p>
-            <textarea />
-            <h2 className="text-2xl mt-4">Perks</h2>
-            <p className="text-2xl mt-4">Select all the pperks of your place</p>
-          </div>
-          <div>
-            <label classname>
-              <input type="checkbox" />
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
-              />
-            </svg>
-
-            <span>Wifi</span>
-            <label>
-              <input type="checkbox" />
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-              />
-            </svg>
-
-            <span>free parking spot</span>
-            <label>
-              <input type="checkbox" />
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
-              />
-            </svg>
-
-            <span>TV</span>
-            <label classname>
-              <input type="checkbox" />
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-              />
-            </svg>
-
-            <span>Pets</span>
-            <label classname>
-              <input type="checkbox" />
-            </label>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25"
-              />
-            </svg>
-
-            <span>Private enterance</span>
-          </div>
-        </form>
-      )}
+        <div className="mt-4">
+          {places.length > 0 && places.map(place => (
+            <Link to={'/account/places/'+place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
+              <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
+                <PlaceImg place={place} />
+              </div>
+              <div className="grow-0 shrink">
+                <h2 className="text-xl">{place.title}</h2>
+                <p className="text-sm mt-2">{place.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
     </div>
   );
 }
