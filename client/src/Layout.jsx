c8e0753fb76f37ba1c651d2a1d497d7b92de1395
Anas Yousif <Anasyousif182@gmail.com>
2023-10-27 07:21:21 -0400

    final commits

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
-export function Layout() {
+export  default function Layout() {
     return(
         <div className="p-4 felx felx-col min-h-screen">
           <Header />
